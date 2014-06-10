956d7957c147072000d64e0c7558e2489a1d7979
AxelHabermaier <mail@axel-habermaier.de>
2014-06-10 08:35:11 +0200

    Added F# projects

--- Source/SafetySharp.CSharp/Properties/AssemblyInfo.cs
+++ Source/SafetySharp.CSharp/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: ComVisible(false)]
-[assembly: InternalsVisibleTo("Tests")]
+[assembly: InternalsVisibleTo("Tests.CSharp")]
 [assembly: InternalsVisibleTo("TestCompilation")]
