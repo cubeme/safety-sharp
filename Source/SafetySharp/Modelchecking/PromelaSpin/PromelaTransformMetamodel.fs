84e436cf7fa353616b899e2f1806ac343adc7d2b
Johannes Leupolz <johannes.leupolz@googlemail.com>
2014-06-13 14:53:44 +0200

    Promela: F# Declaration and Initialisation of Fields in Promela done

--- Source/SafetySharp/Modelchecking/PromelaSpin/PromelaTransformMetamodel.fs
+++ Source/SafetySharp/Modelchecking/PromelaSpin/PromelaTransformMetamodel.fs
@@ -2,10 +2,23 @@
 
 open PromelaAstHelpers
 
+type MMModelObject = SafetySharp.Metamodel.ModelObject
+type MMPartitionObject = SafetySharp.Metamodel.PartitionObject
+type MMComponentObject = SafetySharp.Metamodel.ComponentObject
+type MMFieldObject = SafetySharp.Metamodel.FieldObject
+
+type MMTypeSymbol = SafetySharp.Metamodel.TypeSymbol
+type MMFieldSymbol = SafetySharp.Metamodel.FieldSymbol
+type MMParameterSymbol = SafetySharp.Metamodel.ParameterSymbol
+type MMMethodSymbol = SafetySharp.Metamodel.MethodSymbol
+type MMSubcomponentSymbol = SafetySharp.Metamodel.SubcomponentSymbol
+type MMComponentSymbol = SafetySharp.Metamodel.ComponentSymbol
+type MMPartitionSymbol = SafetySharp.Metamodel.PartitionSymbol
+type MMModelSymbol = SafetySharp.Metamodel.ModelSymbol
+
 type MMExpression = SafetySharp.Metamodel.Expression
 type MMUnaryOperator = SafetySharp.Metamodel.UnaryOperator
 type MMBinaryOperator = SafetySharp.Metamodel.BinaryOperator
-type MMFieldSymbol = SafetySharp.Metamodel.FieldSymbol
 type MMStatement = SafetySharp.Metamodel.Statement
 type MMFormula = SafetySharp.Metamodel.Formula
 type MMUnaryFormulaOperator = SafetySharp.Metamodel.UnaryFormulaOperator
@@ -22,10 +35,102 @@ type PrSequence = PromelaDataStructures.Ast.Sequence
 type PrStep = PromelaDataStructures.Ast.Step
 type PrFormula = PromelaDataStructures.Ast.Formula
 type PrBinaryFormulaOperator = PromelaDataStructures.Ast.BinaryFormulaOperator
-type PrUnaryFormulaOperator =PromelaDataStructures.Ast. UnaryFormulaOperator
+type PrUnaryFormulaOperator = PromelaDataStructures.Ast.UnaryFormulaOperator
+type PrVarref = PromelaDataStructures.Ast.Varref
+type PrProctype = PromelaDataStructures.Ast.Proctype
+type PrDeclLst = PromelaDataStructures.Ast.DeclLst
+type PrModule = PromelaDataStructures.Ast.Module
+type PrOneDecl = PromelaDataStructures.Ast.OneDecl
+type PrTypename = PromelaDataStructures.Ast.Typename
+type PrIvar = PromelaDataStructures.Ast.Ivar
+type PrAssign = PromelaDataStructures.Ast.Assign
 
 
-type MetamodelToPromela() =
+type FieldInfo = {
+    partition : MMPartitionObject
+    container : MMComponentObject list;
+    field : MMFieldObject;
+}
+
+let getSubComponentObjects (subcomponentMap : Map<MMSubcomponentSymbol, MMComponentObject>) : (MMComponentObject list) =
+    subcomponentMap |> Map.fold (fun acc key value -> value::acc) []
+    
+let getFieldObjects (fieldMap : Map<MMFieldSymbol, MMFieldObject>) : (MMFieldObject list) =
+    fieldMap |> Map.fold (fun acc key value -> value::acc) []
+
+
+let collectFields (model : MMModelObject) : FieldInfo list =
+    let rec collectFromComponent (partition:MMPartitionObject) (parents:MMComponentObject list) (comp:MMComponentObject) : FieldInfo list = 
+        let collectedInSubcomponents : FieldInfo list=
+            (getSubComponentObjects comp.Subcomponents) |> List.collect (fun comp -> collectFromComponent partition (comp::parents) comp)
+        let collectFieldInThisComponent (fieldobject:MMFieldObject) =
+            {
+                FieldInfo.partition = partition;
+                FieldInfo.container = comp::parents;
+                FieldInfo.field = fieldobject;
+            }
+        let collectedInThisComponent = (getFieldObjects comp.Fields) |> List.map collectFieldInThisComponent 
+        collectedInThisComponent @ collectedInSubcomponents
+    let collectFromPartition (partition:MMPartitionObject) : FieldInfo list  =
+        collectFromComponent partition [] partition.RootComponent
+    model.Partitions |> List.collect collectFromPartition
+        
+type MetamodelToPromela() =    
+    member this.transformFieldInfoToName (fieldInfo : FieldInfo) =
+        let partitionName = "pA" //partition has no name, use A as dummy. TODO: find something better
+        let componentName =
+            fieldInfo.container |> List.map (fun comp -> comp.Name)
+                                |> List.rev //the order should be root::subcomponent::leafSubcomponent
+                                |> List.fold (fun acc elem -> acc + "_c" + elem) ""
+        let fieldName = "_f"+fieldInfo.field.FieldSymbol.Name
+        sprintf "%s%s%s" partitionName componentName fieldName
+
+    member this.transformFieldInfoToVarref (fieldInfo : FieldInfo) =
+        let varName = this.transformFieldInfoToName fieldInfo
+        PrVarref.Varref(varName,None,None)
+    
+    member this.generateFieldDeclarations (fields : FieldInfo list) : PrModule =
+        let generateDecl (field:FieldInfo) : PrOneDecl =
+            let _type = match field.field.FieldSymbol.Type with
+                            | MMTypeSymbol.Boolean -> PrTypename.Bool
+                            | MMTypeSymbol.Integer -> PrTypename.Int
+                            | MMTypeSymbol.Decimal -> failwith "NotImplementedYet"
+            let _varName = this.transformFieldInfoToName field
+            let _variable = PrIvar.Ivar(_varName,None,None)
+            PrOneDecl.OneDecl(None,_type,[_variable])
+        fields |> List.map generateDecl
+               |> PrDeclLst.DeclLst
+               |> PrModule.GlobalVarsAndChans
+
+    member this.generateFieldInitialisations (fields : FieldInfo list) : PrStatement list =
+        let generateInit (field:FieldInfo) : PrStatement =
+            let generateSequence (initialValue : obj) : PrSequence =
+                let assignVarref = this.transformFieldInfoToVarref field
+                let assignExpr =
+                    match initialValue with
+                        | :? int as value -> PrExpression.Const(PrConst.Number(value))
+                        | :? bool as value -> match value with
+                                                | true  -> PrExpression.Const(PrConst.True)
+                                                | false -> PrExpression.Const(PrConst.False)
+                        | _ -> failwith "NotImplementedYet"
+                let assignmentStep =
+                    PrStep.StmntStep(PrStatement.AssignStmnt(PrAssign.AssignExpr(assignVarref,assignExpr)),None)
+                PrSequence.Sequence([assignmentStep])
+            field.field.InitialValues |> List.map generateSequence
+                                      |> PrOptions.Options
+                                      |> PrStatement.IfStmnt
+        fields |> List.map generateInit
+        
+
+    member this.generatePartitionUpdateCode =
+        []
+
+    member this.generatePartitionBindingCode =
+        ""
+
+    member this.transformModelObject (model:MMModelObject) : PrProctype =
+        ""
+
 
     member this.transformExpression (expression:MMExpression) : PrExpression =
         match expression with
@@ -104,3 +209,5 @@ type MetamodelToPromela() =
                     | MMBinaryFormulaOperator.Equivalence -> PrFormula.BinaryFormula(transformedLeft,PrBinaryFormulaOperator.Equals,transformedRight)
                     | MMBinaryFormulaOperator.Until       -> PrFormula.BinaryFormula(transformedLeft,PrBinaryFormulaOperator.Until,transformedRight)
                     | _ -> failwith "No CTL available"
+    
+    
