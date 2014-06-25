23b91b374f5bb68ef19c0be5b93eb8b7f1259433
Johannes Leupolz <johannes.leupolz@googlemail.com>
2014-06-25 16:33:57 +0200

    NuXmv: Organistation of the WriteOnceTypeFieldManager improved

--- Source/SafetySharp/Modelchecking/Common/WriteOnceStatement.fs
+++ Source/SafetySharp/Modelchecking/Common/WriteOnceStatement.fs
@@ -86,9 +86,14 @@ type WriteOnceStatement =
 
 
 type WriteOnceTypeFieldManager = {
+    // static across a model
     SimpleFieldToInitialFieldMapping : Map<SimpleGlobalFieldWithContext,SimpleGlobalField list>; //map to the initial SimpleGlobalField
-    SimpleFieldToCounterMapping : Map<SimpleGlobalFieldWithContext,int> ref; //every time a new artificial field is created, counter is increased. It is "ref" so its value is shared across different instances leading back to the same .Initialize
-    SimpleFieldToCurrentArtificialFieldMapping : Map<SimpleGlobalFieldWithContext,SimpleGlobalField list>; //if an artificial field for a reference field is created, put the new field on the stack of artificial fields of this reference field
+    // static across a scope. Changes with scopes. Organised as stacks. The head elements are the current elements. on a popScope the head elements are just thrown away
+    SimpleFieldToCurrentArtificialFieldMapping : (Map<SimpleGlobalFieldWithContext,SimpleGlobalField>) list; //if an artificial field for a reference field is created, link the artificial fields to the field it overwrites
+    SimpleFieldToNewArtificialFieldMapping : (Map<SimpleGlobalFieldWithContext,SimpleGlobalField>) list; //same as above, but only the fields introduced in this of the current scope. If a field is not "overwritten" in this scope, the map does not contain it.
+    // Following items are shared across all managers with the same .Initialize. They should only be changed internally.
+    SimpleFieldToArtificialCounterShared : Map<SimpleGlobalFieldWithContext,int> ref; //every time a new artificial field is created, counter is increased. It is "ref" so its value is shared across different instances leading back to the same .Initialize
+    CreatedArtificialFieldsShared : (SimpleGlobalField list) ref; //every time a new artificial field is created by the manager, it is associated with this manager. It is "ref" so its value is shared across different instances leading back to the same .Initialize
 
 } with
     static member Initialize (fieldsOfPartition:SimpleGlobalField list) =
@@ -100,6 +105,10 @@ type WriteOnceTypeFieldManager = {
         {
             WriteOnceTypeFieldManager.CurrentMapping = ref initializeCurrentValueMap
         }
+    member this.pushScope : WriteOnceTypeFieldManager =
+        this
+    member this.popScope : WriteOnceTypeFieldManager =
+        this
     member this.transformExpressionWithCurrentRedirections (expression:SimpleExpression) : WriteOnceExpression =
         //TODO: Take current redirections of fields in fieldManager into account
         expression
@@ -118,10 +127,16 @@ type SimpleStatementsToWriteOnceStatements =
     //  The fieldManager keeps the information of the current redirections and allows to introduce new unique artificial variables
     // returns the converted Statements and a list of variables, which got touched in the conversion progress
     member this.simpleStatementToWriteOnceStatementsCached (takenDecisions:SimpleExpression list) (fieldManager:WriteOnceTypeFieldManager) (stmnts:SimpleStatement list) : (WriteOnceStatement list*WriteOnceTypeFieldManager) =
-        let statementsToMergeTaintedVariables (fieldManagers:WriteOnceTypeFieldManager list) (takenDecisions:SimpleExpression list) : (WriteOnceStatement list*WriteOnceTypeFieldManager) =
+        let statementsToMergeTaintedVariables (fieldManagersToMerge:WriteOnceTypeFieldManager list) (takenDecisions:SimpleExpression list) : (WriteOnceStatement list*WriteOnceTypeFieldManager) =
             //Code, which allows merging of the tainted Variables on a return
-            ([],newFieldManager)
-
+            
+            let thingsToMerge = fieldManagers |> List.fold (fun acc fieldManager -> fieldManager.)
+            ([])
+            // new fieldManager contains newly introduced artificial field
+            // problem: every fieldManager looks like this: (fieldsOfBranch@fieldsOfContext). We are only interested in fieldsOfBranch1, fieldsOfBranch2,...
+            //          we must find a way to only get these
+            // solutions: - organize fieldManager as Stack
+            //            - new argument?!?
         let transformSimpleStatement (fieldManager:WriteOnceTypeFieldManager) (statement:SimpleStatement) : (WriteOnceStatement list*WriteOnceTypeFieldManager) =
             match statement with
                 | SimpleStatement.GuardedCommandStatement (optionsOfGuardedCommand:(( SimpleExpression * (SimpleStatement list) ) list)) -> //Context * Guard * Statements  
@@ -136,7 +151,7 @@ type SimpleStatementsToWriteOnceStatements =
                                                                                            |> List.unzip
                     let (codeOfBranchesAggregated) = codeOfBranches |> List.concat
                     // MERGE BRANCHES
-                    let (codeToMergeBranches,fieldManagerAfterMerge) = statementsToMergeTaintedVariables fieldManagersAfterSplit takenDecisions
+                    let (codeToMergeBranches,fieldManagerAfterMerge) = statementsToMergeTaintedVariables fieldManagersAfterSplit takenDecisions                    
                     // concat Code of every BRANCH and Code to MERGE
                     (codeOfBranchesAggregated @ codeToMergeBranches,fieldManagerAfterMerge)
 
