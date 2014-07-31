9dc8cb4fdcbe45099a9f9d0b71ace5be2d5b83ae
Johannes Leupolz <johannes.leupolz@googlemail.com>
2014-07-31 11:29:35 +0200

    NuXmv: FindNuXmv-function

--- Source/SafetySharp/Modelchecking/NuXmv/NuXmvExecute.fs
+++ Source/SafetySharp/Modelchecking/NuXmv/NuXmvExecute.fs
@@ -22,5 +22,67 @@
 
 namespace SafetySharp.Internal.Modelchecking.NuXmv
 
-module internal NuXmvExecute =
-    let execute = ""
+
+open System.Diagnostics
+
+type internal NuXmvResult =
+    | Successful of string * string
+    | Failed of string * string
+    with
+        member this.HasSucceeded with get () =
+                                    match this with
+                                        | Successful (_,_) -> true
+                                        | Failed (_,_) -> false
+
+type internal ExecuteNuXmv() =
+    static member FindNuXmv (): string =
+        let tryCandidate (filename:string) : bool =
+            System.IO.File.Exists filename
+        let candidatesManual = [
+            "..\\..\\..\\..\\Dependencies\\NuXmv.exe";
+        ]
+        let candidatesOfPath =
+            let paths=System.Environment.GetEnvironmentVariable("PATH").Split(';')
+            paths |> Array.map (fun path -> System.IO.Path.Combine(path,"NuXmv.exe"))
+                  |> Array.toList
+        let candidates = candidatesManual @ candidatesOfPath
+        match candidates |> Seq.tryFind tryCandidate with
+            | Some(filename) -> filename
+            | None -> failwith "Please add NuXmv installation folder into PATH or copy NuXmv-executable into the dependency folder"
+
+    static member ExecuteNuXmv (arguments:string) : NuXmvResult =
+        let stdoutOutputBuffer = new System.Text.StringBuilder ()
+        let stderrOutputBuffer = new System.Text.StringBuilder ()
+        use proc = new Process()
+        
+        proc.StartInfo.Arguments <- arguments
+        proc.StartInfo.FileName <- ExecuteNuXmv.FindNuXmv ()
+        proc.StartInfo.WindowStyle <-  ProcessWindowStyle.Hidden
+        proc.StartInfo.CreateNoWindow <-  true
+        proc.StartInfo.UseShellExecute <-  false
+        proc.StartInfo.RedirectStandardOutput <-  true
+        proc.StartInfo.RedirectStandardError <-  true
+        proc.StartInfo.RedirectStandardInput <-  true
+
+
+        //proc.EnableRaisingEvents = true |> ignore
+        // TODO: hier könnte man noch zu den Buffern einen TimeCode oder sowas hinzufügen
+        proc.ErrorDataReceived.Add (fun dataReceivedEvArgs -> (stderrOutputBuffer.Append(dataReceivedEvArgs.Data) |> ignore) )
+        proc.OutputDataReceived .Add (fun dataReceivedEvArgs -> (stdoutOutputBuffer.Append(dataReceivedEvArgs.Data) |> ignore))
+
+        proc.Start() |> ignore
+        
+        proc.BeginErrorReadLine ()
+        proc.BeginOutputReadLine ()
+
+        // proc.StandardInput.WriteLine("hier Eingabe für das Programm");
+        // proc.StandardInput.Flush();
+        
+        proc.WaitForExit()
+
+        //let (output:string) = proc.StandardOutput.ReadToEnd() <--- braucht man wohl nicht mehr mit den asynchronen Funktionen, die später vllt. nützlich werden können
+
+        let exitCode = proc.ExitCode
+        match exitCode with
+            | 0 -> Successful(stdoutOutputBuffer.ToString(), stderrOutputBuffer.ToString())
+            | _ -> Failed(stdoutOutputBuffer.ToString(), stderrOutputBuffer.ToString())
