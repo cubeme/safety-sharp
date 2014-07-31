466f1a27a65d591ab5fb3290c426509d75657fe5
Johannes Leupolz <johannes.leupolz@googlemail.com>
2014-07-31 17:03:00 +0200

    NuXmv: Interesting commands added

---
+++ Source/SafetySharp/Modelchecking/NuXmv/NuXmvCommands.fs
@@ -0,0 +1,144 @@
+﻿// The MIT License (MIT)
+// 
+// Copyright (c) 2014, Institute for Software & Systems Engineering
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+namespace SafetySharp.Internal.Modelchecking.NuXmv
+
+type internal IEnvironmentVariable =
+    interface end
+
+type internal ICommand =
+    interface end
+
+// Chapter 4
+
+// Note: We only introduce commands, which might be useful for us
+
+[<RequireQualifiedAccess>]
+type internal NuSMVCommand =
+    // Chapter 4.1 Model Reading and Building
+    | ReadModel of FileName:string
+    | FlattenHierarchy
+    | ShowVars
+    | ShowDependencies of Expression:NextExpression
+    | EncodeVariables
+    | BuildModel
+    | Go
+    | BuildFlatModel
+    // Chapter 4.2 Commands for Checking Specifications
+    // Chapter 4.3 Commands for Bounded Model Checking
+    // Chapter 4.4 Commands for checking PSL specifications
+    // Chapter 4.5 Simulation Commands
+    // Chapter 4.6 Execution Commands
+    // Chapter 4.7 Traces
+    // Chapter 4.8 Trace Plugins
+    // Chapter 4.9 Interface to the DD Package
+    // Chapter 4.10 Administration Commands
+    | Echo of Variable:string
+    | EchoTyped of Variable:IEnvironmentVariable // Typed version of "Echo" for convenience
+    | PrintUsage
+    | Quit
+    | Reset
+    | Set of Name:string * Value:(string option)
+    | SetTyped of IEnvironmentVariable // Typed version of "Set" for convenience
+    | GetAllEnvironmentalVariables // Set without parameters
+    | Unset of Name:string
+    | UnsetTyped of IEnvironmentVariable
+    | Usage
+    with interface ICommand
+
+// Chapter 4.11 Other Environment Variables
+module internal NuSMVEnvironmentVariables =    
+    [<RequireQualifiedAccess>]
+    type NusmvStdErr = {
+            FileName:string;
+        }
+        with interface IEnvironmentVariable
+
+    [<RequireQualifiedAccess>]
+    type NusmvStdout = {
+            FileName:string;
+        }
+        with interface IEnvironmentVariable
+
+    [<RequireQualifiedAccess>]
+    type NusmvStdIn = {
+            FileName:string;
+        }
+        with interface IEnvironmentVariable
+
+[<RequireQualifiedAccess>]
+type internal NuXmvCommand =
+    // Chapter 5.1 Model Simulation
+    | MsatPickState
+    | MsatSimulate
+    // Chapter 5.2 Invariant Checking
+    | MsatCheckInvarBmc
+    | CheckInvarBmcItp
+    | CheckInvarIc3
+    // ... many more to evaluate
+    // Chapter 5.3 LTL Model Checking
+    | MsatCheckLtlspecBmc
+    | CheckLtlspecKlive
+    | CheckLtlspecSimplify
+    | CheckLtlspecCompositional
+    // ... many more to evaluate
+    // Chapter 5.4 Computing Reachable States
+    // Chapter 5.5 Reasoning via Abstraction
+    // Chapter 5.6 Format Conversions
+    // Chapter 5.7 Model Transformation
+    | WriteSimplifiedModelRel //<----- Check this out
+    | WriteSimplifiedModelFunc  //<----- Check this out. I don't get the difference between both simplifiers
+    | BuildSimplifiedProperty
+    with interface ICommand
+
+// Chapter 5.8 Environment Variables
+module internal NuXmvEnvironmentVariables = 
+    // Cone Of Influence
+    
+    [<RequireQualifiedAccess>]
+    type AbstractionEngine =
+        | Msat
+        | Structural
+        | Hybrid
+        | Bdd
+        with interface IEnvironmentVariable
+        
+[<RequireQualifiedAccess>]
+type internal NuXmvCommandLine =
+    | Help
+    | Verbose
+    | Interactive
+    | AvoidLoadingDefaultSettings // -s
+
+module internal NuXmvHelpfulCommandSequences =
+    let readmodel (filename: string ) = [
+        NuSMVCommand.ReadModel filename
+    ]
+    let switchToXmlOutput = [
+    ]
+    let goCommandExpanded (filename:string) = [
+        NuSMVCommand.ReadModel filename;
+        NuSMVCommand.FlattenHierarchy;
+        NuSMVCommand.EncodeVariables;
+        NuSMVCommand.BuildFlatModel;
+        NuSMVCommand.BuildModel;
+    ]
---
+++ Source/SafetySharp/Modelchecking/NuXmv/NuXmvCommandsToString.fs
@@ -0,0 +1,129 @@
+﻿// The MIT License (MIT)
+// 
+// Copyright (c) 2014, Institute for Software & Systems Engineering
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+namespace SafetySharp.Internal.Modelchecking.NuXmv
+
+
+type internal ExportCommandsToFile() =
+    
+    let astToFile = ExportNuXmvAstToFile()
+
+    member this.ExportNameOfEnvironmentVariable (variable:IEnvironmentVariable) : string =
+        match variable with
+            // NuSMV
+            | :? NuSMVEnvironmentVariables.NusmvStdErr as stderr -> "nusmv_stderr"
+            | :? NuSMVEnvironmentVariables.NusmvStdout as stdout -> "nusmv_stdout"
+            | :? NuSMVEnvironmentVariables.NusmvStdIn as stdin -> "nusmv_stdin"
+            // NuXmv
+            | :? NuXmvEnvironmentVariables.AbstractionEngine as abstractionEngine -> "abstraction.engine"
+            // NotImplementedYet
+            | _ -> failwith "NotImplementedYet"
+
+    member this.ExportNuSMVCommand (command:NuSMVCommand) : string =
+        match command with
+            | NuSMVCommand.ReadModel (fileName:string) ->
+                 sprintf "read_model -i %s" fileName
+            | NuSMVCommand.FlattenHierarchy ->
+                "flatten_hierarchy"
+            | NuSMVCommand.ShowVars -> "show_vars"
+            | NuSMVCommand.ShowDependencies (expression:NextExpression) ->
+                sprintf "show_dependencies -e %s" (astToFile.ExportNextExpression expression)
+            | NuSMVCommand.EncodeVariables ->
+                "encode_variables"
+            | NuSMVCommand.BuildModel ->
+                "build_model"
+            | NuSMVCommand.Go ->
+                "go"
+            | NuSMVCommand.BuildFlatModel ->
+                "build_flat_model"
+                
+            | NuSMVCommand.Echo (variable:string) ->
+                sprintf "echo %s" variable
+            | NuSMVCommand.EchoTyped (variable:IEnvironmentVariable) ->
+                sprintf "echo %s" (this.ExportNameOfEnvironmentVariable variable)
+            | NuSMVCommand.PrintUsage ->
+                "print_usage"
+            | NuSMVCommand.Quit ->
+                "quit"
+            | NuSMVCommand.Reset ->
+                "reset"
+            | NuSMVCommand.Set (name:string,value:string option) ->
+                let valueStr =
+                    match value with
+                        | Some(value) -> sprintf " %s" value
+                        | None -> ""
+                sprintf "set %s%s" name valueStr
+            
+            | NuSMVCommand.SetTyped (variable:IEnvironmentVariable)  ->
+                match variable with
+                    // NuSMV
+                    | :? NuSMVEnvironmentVariables.NusmvStdErr as stderr ->
+                        sprintf "set nusmv_stderr %s" stderr.FileName
+                    | :? NuSMVEnvironmentVariables.NusmvStdout as stdout ->
+                        sprintf "set nusmv_stdout %s" stdout.FileName
+                    | :? NuSMVEnvironmentVariables.NusmvStdIn as stdin ->
+                        sprintf "set nusmv_stdin %s" stdin.FileName
+                    // NuXmv
+                    | :? NuXmvEnvironmentVariables.AbstractionEngine as abstractionEngine ->
+                        let engine = match abstractionEngine with
+                                        | NuXmvEnvironmentVariables.AbstractionEngine.Msat -> "msat"
+                                        | NuXmvEnvironmentVariables.AbstractionEngine.Structural -> "structural"
+                                        | NuXmvEnvironmentVariables.AbstractionEngine.Hybrid -> "hybrid"
+                                        | NuXmvEnvironmentVariables.AbstractionEngine.Bdd -> "bdd"
+                        sprintf "set abstraction.engine %s" engine
+                    // NotImplementedYet
+                    | _ -> failwith "NotImplementedYet"
+            | NuSMVCommand.GetAllEnvironmentalVariables ->
+                "set" // Set without parameters
+            
+            | NuSMVCommand.Unset (name:string) ->
+                sprintf "unset %s" name
+            
+            | NuSMVCommand.UnsetTyped (variable:IEnvironmentVariable)  ->
+                sprintf "unset %s" (this.ExportNameOfEnvironmentVariable variable)
+            | NuSMVCommand.Usage ->
+                "usage"
+
+    member this.ExportNuXmvCommand (command:NuXmvCommand) : string =
+        match command with
+            | NuXmvCommand.MsatPickState             -> failwith "NotImplementedYet"
+            | NuXmvCommand.MsatSimulate              -> failwith "NotImplementedYet"
+
+            | NuXmvCommand.MsatCheckInvarBmc         -> failwith "NotImplementedYet"
+            | NuXmvCommand.CheckInvarBmcItp          -> failwith "NotImplementedYet"
+            | NuXmvCommand.CheckInvarIc3             -> failwith "NotImplementedYet"
+
+            | NuXmvCommand.MsatCheckLtlspecBmc       -> failwith "NotImplementedYet"
+            | NuXmvCommand.CheckLtlspecKlive         -> failwith "NotImplementedYet"
+            | NuXmvCommand.CheckLtlspecSimplify      -> failwith "NotImplementedYet"
+            | NuXmvCommand.CheckLtlspecCompositional -> failwith "NotImplementedYet"
+
+            | NuXmvCommand.WriteSimplifiedModelRel   -> failwith "NotImplementedYet"
+            | NuXmvCommand.WriteSimplifiedModelFunc  -> failwith "NotImplementedYet"
+            | NuXmvCommand.BuildSimplifiedProperty   -> failwith "NotImplementedYet"
+
+    member this.ExportNuXmvCommandLine (argument:NuXmvCommandLine) : string =
+        match argument with
+            | NuXmvCommandLine.Help                        -> "-help"
+            | NuXmvCommandLine.Verbose                     -> "-v 1"
+            | NuXmvCommandLine.Interactive                 -> "-int"
+            | NuXmvCommandLine.AvoidLoadingDefaultSettings -> "-s"
