adaed2948bfe94df6ade42d3444f504abce78653
Johannes Leupolz <johannes.leupolz@googlemail.com>
2014-09-22 15:25:58 +0200

    Prism: Rewrote IsPrismRunable

--- Source/SafetySharp/Modelchecking/Prism/PrismExecute.fs
+++ Source/SafetySharp/Modelchecking/Prism/PrismExecute.fs
@@ -24,8 +24,30 @@ namespace SafetySharp.Internal.Modelchecking.Prism
 
 open SafetySharp.Internal.Utilities
 
-type internal ExecutePrism() =
+type internal ExecutePrism =
+    val private arguments : string
+    val stdoutOutputBuffer : System.Text.StringBuilder
+    val stderrOutputBuffer : System.Text.StringBuilder
     
+    val mutable processOutputReader : System.Threading.Tasks.Task
+    val mutable processErrorReader : System.Threading.Tasks.Task
+    val mutable processWaiter : System.Threading.Tasks.Task
+
+    val proc : System.Diagnostics.Process
+
+    new (arguments : string) as this =
+        {
+            arguments = arguments;
+            stdoutOutputBuffer = new System.Text.StringBuilder ();
+            stderrOutputBuffer = new System.Text.StringBuilder ();
+            processOutputReader = null;
+            processErrorReader = null;
+            processWaiter = null;
+            proc = new System.Diagnostics.Process()
+        }
+        then
+            this.ExecutePrismWithArgument()
+
 
     static member FindJava () : string =
         let tryCandidate (filename:string) : bool =
@@ -77,6 +99,8 @@ type internal ExecutePrism() =
         if List.exists (fun elem -> elem = prismLibDir) prismCandidatesInPath then
             ()
         else
+            // Add libdir to PATH
+            // http://stackoverflow.com/questions/2998343/adding-a-directory-temporarily-to-windows-7s-dll-search-paths
             System.Environment.SetEnvironmentVariable("PATH",System.Environment.GetEnvironmentVariable("PATH")+";"+prismLibDir);
 
         let fileNameToPrismDll = System.IO.Path.Combine(prismDir,"lib","prism.dll")
@@ -85,111 +109,73 @@ type internal ExecutePrism() =
            let failString = sprintf "java VM and prism version are not compiled for the same architecture. Please replace prism by a version compiled for java version of %s (%s)" javaExe (javaMachineCode.ToString())
            failwith failString
         prismDir
-
-    static member argumentForJava (arguments:string) : string =
-        let prismDir = ExecutePrism.FindPrismAndAddToPath ()
-        let classpath =
-            let stringFromBat = """%PRISM_DIR%\lib\prism.jar;%PRISM_DIR%\classes;%PRISM_DIR%;%PRISM_DIR%\lib\pepa.zip;%PRISM_DIR%\lib\*"""
-            stringFromBat.Replace("%PRISM_DIR%",prismDir)
-        sprintf """-Djava.library.path="%s\lib" -classpath "%s" prism.PrismCL %s""" prismDir classpath arguments
-
-    static member IsPrismRunable () : bool =        
+            
+    member private this.ExecutePrismWithArgument ()  =
+        let argumentForJava (arguments:string) : string =
+            let prismDir = ExecutePrism.FindPrismAndAddToPath ()
+            let classpath =
+                let stringFromBat = """%PRISM_DIR%\lib\prism.jar;%PRISM_DIR%\classes;%PRISM_DIR%;%PRISM_DIR%\lib\pepa.zip;%PRISM_DIR%\lib\*"""
+                stringFromBat.Replace("%PRISM_DIR%",prismDir)
+            sprintf """-Djava.library.path="%s\lib" -classpath "%s" prism.PrismCL %s""" prismDir classpath arguments            
         let javaExe = ExecutePrism.FindJava ()
-        use proc = new System.Diagnostics.Process()        
-        proc.StartInfo.Arguments <- ExecutePrism.argumentForJava "-help"
-        proc.StartInfo.FileName <- javaExe
-        proc.StartInfo.WindowStyle <-  System.Diagnostics.ProcessWindowStyle.Hidden
-        proc.StartInfo.CreateNoWindow <-  true
-        proc.StartInfo.UseShellExecute <-  false
-        //proc.StartInfo.RedirectStandardOutput <-  true
-        //proc.StartInfo.RedirectStandardError <-  true
-        proc.StartInfo.RedirectStandardInput <-  true
-        //proc.StartInfo.WorkingDirectory
-        proc.Start() |> ignore
-        proc.StandardInput.AutoFlush <- true
+        this.proc.StartInfo.Arguments <- argumentForJava this.arguments
+        this.proc.StartInfo.FileName <- javaExe
+        this.proc.StartInfo.WindowStyle <-  System.Diagnostics.ProcessWindowStyle.Hidden
+        this.proc.StartInfo.CreateNoWindow <-  true
+        this.proc.StartInfo.UseShellExecute <-  false
+        this.proc.StartInfo.RedirectStandardOutput <-  true
+        this.proc.StartInfo.RedirectStandardError <-  true
+        this.proc.StartInfo.RedirectStandardInput <-  true
+        this.proc.Start() |> ignore
+        this.proc.StandardInput.AutoFlush <- true
+        this.processOutputReader <- this.TaskReadStdout ()
+        this.processErrorReader <- this.TaskReadStderr ()
+        this.processWaiter <- this.TaskWaitForEnd (0)
+        ()
         
-        proc.WaitForExit ()
-        
-        // TODO lib needs to be added to PATH, otherwise prism can't find required dll-files
-        // http://stackoverflow.com/questions/2998343/adding-a-directory-temporarily-to-windows-7s-dll-search-paths
 
-        //let stderr = proc.StandardError.ReadToEnd()
-        //let stdout = proc.StandardOutput.ReadToEnd()
-
-        let exitCode = proc.ExitCode
+    static member IsPrismRunable () : bool =
+        let executePrism = ExecutePrism("-help")
+        let exitCode = executePrism.WaitUntilPrismTerminates ()
         // under windows there are no negative values. Thus exit code -1 becomes 255 (2-complement)
         match exitCode with
             | 0 -> true
             | _ -> false
 
-    (* 
-    member this.TaskReadStderr () : System.Threading.Tasks.Task =
+    member this.IsPrismRunning () =
+        false
+    
+    
+    member this.WaitUntilPrismTerminates () =
+        System.Threading.Tasks.Task.WaitAll(this.processOutputReader,this.processErrorReader,this.processWaiter)
+        this.proc.ExitCode
+
+    member this.GetNextResult () =
+        ()
+
+    member private this.TaskReadStderr () : System.Threading.Tasks.Task =
         System.Threading.Tasks.Task.Factory.StartNew(
             fun () -> 
-                // http://msdn.microsoft.com/en-us/library/system.io.streamreader.readline(v=vs.110).aspx
-                while proc.StandardError.EndOfStream <> true  do
-                    let newLine = proc.StandardError.ReadLine()
-                    if newLine.StartsWith commandEndingStringStderr then
-                        stderrOutputBuffer.AppendLine newLine |> ignore  //remove, if marker should not appear in stderr
-                        stderrFinishedBlocker.Set () |> ignore
-                    else
-                        stderrOutputBuffer.AppendLine newLine |> ignore
-                stderrFinishedBlocker.Set () |> ignore
+                while this.proc.StandardError.EndOfStream <> true  do
+                    let newLine = this.proc.StandardError.ReadLine()
+                    this.stderrOutputBuffer.AppendLine newLine |> ignore
         )
                 
-    member this.TaskReadStdout () : System.Threading.Tasks.Task =
-        let FinishCommandAndReleaseBlocker () =
-            let newFinishedCommand = {
-                NuXmvCommandResultBasic.Command = activeCommand.Value;
-                NuXmvCommandResultBasic.Stdout = stdoutOutputBuffer.ToString();
-                NuXmvCommandResultBasic.Stderr = stderrOutputBuffer.ToString();
-                NuXmvCommandResultBasic.Failure = None; //Assume no failure occurred. If a failure occurred, ExecuteCommand detects it and corrects the result
-            }
-            lastCommandResult <-  Some(newFinishedCommand)
-            activeCommand <- None
-            stdoutOutputBuffer.Clear() |> ignore
-            stderrOutputBuffer.Clear() |> ignore
-            stdoutAndCommandFinishedBlocker.Set() |> ignore
+    member private this.TaskReadStdout () : System.Threading.Tasks.Task =
         System.Threading.Tasks.Task.Factory.StartNew(
             fun () -> 
-                let mutable needToRemovePromptFromCurrentLine = false //Before the first command is no prompt
-                while proc.StandardOutput.EndOfStream <> true do
-                    let newLine = proc.StandardOutput.ReadLine()
-                    let newLineCleared =
-                        // we need to clean out the prompt after a new command
-                        // otherwise the check "newLine.StartsWith commandEndingStringStdout"
-                        // is not true, when the prompt is before the marker
-                        if needToRemovePromptFromCurrentLine && newLine.StartsWith(promptString) then
-                            needToRemovePromptFromCurrentLine <- false
-                            newLine.Remove(0,promptString.Length)
-                        else
-                            newLine
-                    if newLineCleared.StartsWith commandEndingStringStdout then
-                        stdoutOutputBuffer.AppendLine newLineCleared |> ignore //remove, if marker should not appear in stdout
-                        stderrFinishedBlocker.WaitOne() |> ignore
-                        FinishCommandAndReleaseBlocker ()
-                        needToRemovePromptFromCurrentLine <- true
-                        ()
-                    else                        
-                        stdoutOutputBuffer.AppendLine newLineCleared |> ignore
-                // process definitively terminated here, because otherwise we wouldn't have received a EndOfStream-Token
-                // we set the currentModeOfProgram here, because only here we can assure, that
-                //    * we read everything from stderr
-                //    * the main thread still waits in ExecuteCommand and there is no race-condition between the
-                //      current command and a next command, which cannot be processed anymore, because nuXmv terminated.
-                currentModeOfProgram<-NuXmvModeOfProgramm.Terminated //must occur before FinishCommandAndReleaseBlocker
-                stderrFinishedBlocker.WaitOne() |> ignore
-                FinishCommandAndReleaseBlocker ()
-                ()
+                while this.proc.StandardOutput.EndOfStream <> true  do
+                    let newLine = this.proc.StandardOutput.ReadLine()
+                    this.stdoutOutputBuffer.AppendLine newLine |> ignore
         )
 
-    member this.TaskWaitForEnd (timeInMs:int) : System.Threading.Tasks.Task<bool> =
+    member private this.TaskWaitForEnd (timeInMs:int) : System.Threading.Tasks.Task<bool> =
         System.Threading.Tasks.Task<bool>.Factory.StartNew(
             fun () ->
                 if timeInMs > 0 then
-                    let result = proc.WaitForExit(timeInMs)
+                    let result = this.proc.WaitForExit(timeInMs)
                     result
                 else
-                    proc.WaitForExit()
+                    this.proc.WaitForExit()
                     true
-        )*)
+        )
