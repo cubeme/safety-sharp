a6fbb09a9760c13a67dacdfa17440abb83cd8148
Johannes Leupolz <johannes.leupolz@googlemail.com>
2014-10-22 19:05:20 +0200

    Stochastic Analysis: Some types added for probabilistic distributions

--- Source/SafetySharp/Analysis/UnitsOfMeasurement.fs
+++ Source/SafetySharp/Analysis/UnitsOfMeasurement.fs
@@ -20,5 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-module UnitsOfMeasurement
+namespace SafetySharp.Internal.Analysis
 
+type PointOfTime = 
+    class end
