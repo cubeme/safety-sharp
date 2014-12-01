cc83bdfc38b5636fd4d87dcbc4b9d4276b05506d
Johannes Leupolz <johannes.leupolz@googlemail.com>
2014-12-01 18:10:26 +0100

    Added fparsec-operators, which will be rewritten to save the position of returned parts

--- Source/SafetySharp/ModelFile/ModelFileParser.fs
+++ Source/SafetySharp/ModelFile/ModelFileParser.fs
@@ -105,10 +105,24 @@ module internal ParseModelFile =
                 }
     
     type GuardedCommandClause = Expr * Stm
-    
+
+
+    //these are overwritten variants, which save the position of the parsed identifier into a map
+    let (.>>) p1 p2 =
+        p1 >>= fun x -> p2 >>% x
+
+    let (>>.) p1 p2 =
+        p1 >>= fun _ -> p2
+
+    let (.>>.) p1 p2 =
+        p1 >>= fun a ->
+        p2 >>= fun b -> preturn (a, b)
+
     let pipe7 p1 p2 p3 p4 p5 p6 p7 f =
         pipe4 p1 p2 p3 (tuple4 p4 p5 p6 p7)
             (fun x1 x2 x3 (x4, x5, x6, x7) -> f x1 x2 x3 x4 x5 x6 x7)
+
+
 
 
     // parses the Boolean constants true or false, yielding a Boolean AST node
