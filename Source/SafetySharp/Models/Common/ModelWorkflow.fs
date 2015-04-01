7d50d8d567928b9711374bbdb2a024924f0f0c90
Johannes Leupolz <johannes.leupolz@googlemail.com>
2015-04-01 18:09:14 +0200

    Tracking of variables: Conserve idea with functions and closures, before we try a simpler idea with maps only

--- Source/SafetySharp/Models/Common/ModelWorkflow.fs
+++ Source/SafetySharp/Models/Common/ModelWorkflow.fs
@@ -39,3 +39,62 @@ module internal ModelWorkflow =
         let! (model,trackingInfo) = getState
         return trackingInfo
     }
+    
+    // The transformation of a model goes from source to sink (e.g. from Scm to NuXmv).
+    // It has several intermediate steps. Every such step transforms from untransformed to transformed.
+    // e.g.:
+    //   - leveling SCM up transforms SCM to a flat SCM
+    //   - converting a flat scm into sam.
+    // Every intermediate transformation should also provide a function, which provides a transformation 
+
+    // Each step for model transformation should not only update the model, but also the TrackingInfo.
+    // It needs to provide a new function "trackForwardFromSource" which it can gain by
+    // appending the old "trackForwardFromSource<'source,'untransformed>" to
+    // its own "trackForwardIntermediate"<'untransformed,'transformed>. 
+    // "trackForwardFromSource >>= trackForwardIntermediate" should be <'source,'transformed>.
+    // The function "trackForwardIntermediate" should contain the forwarding map
+    // (Map<'untransformed,'untransformed>) in its closure.
+    
+    type TrackVariableForwardFunction<'source,'transformed> = 'source -> 'transformed
+
+    type TrackingInfo<'varSource,'varTarget when 'varSource : comparison and 'varTarget : comparison> = {
+        ForwardVariables : Map<'varSource,'varTarget>;
+    }
+        with
+            member this.applyIntermediateForward (TrackVariableForwardFunction)
+    
+
+    type TrackingInfoBuilder<'source,'sink> = {
+    }
+        with
+            member this.createTrackingInfo (varsToTrack:'source list) =
+                //TODO: Create a map which summarizes all infos
+                ()
+
+    type TrackingInfoBuilder with
+        static member forwardVariable (functionOnLeft:AstToStringStateFunction)
+                                      (:AstToStringStateFunction)
+                                      (state:AstToStringState) : TrackingInfo =
+            let newState = m state
+            f newState
+
+    // TODO: This function is used to split the TrackingInfo during a workflow (conserve the current TrackingInfo start a new one)
+    // E.g:
+    //   workflow {
+    //       do! readModelA
+    //       // now a fresh TrackingInfo started. It was created by readModelA
+    //       do! transformAtoB
+    //       do! transformBtoC
+    //       let! trackingInfoAtoC = startFreshTrackingInfo
+    //       // now a fresh TrackingInfo started
+    //       let! transformCtoD
+    //       let! transformDtoE
+    //       let! trackingInfoCtoE = startFreshTrackingInfo
+    //       // now a fresh TrackingInfo started
+    //       do! mergeTrackingInfoMany [trackingInfoAtoC;trackingInfoCtoE]
+    //       // now TrackingInfo goes form A to E again
+    //   }
+    //let startFreshTrackingInfo = 
+
+
+
