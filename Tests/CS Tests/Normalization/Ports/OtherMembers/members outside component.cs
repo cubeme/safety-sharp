77bb165f515e490fbc06b2470c5eecbe884bf5cd
Axel Habermaier <mail@axel-habermaier.de>
2015-06-05 16:35:49 +0200

    some more tests

--- Tests/CS Tests/Normalization/Ports/OtherMembers/members outside component.cs
+++ Tests/CS Tests/Normalization/Ports/OtherMembers/members outside component.cs
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-namespace Tests.Normalization.Ports
+namespace Tests.Normalization.Ports.OtherMembers
 {
 	using System;
 
